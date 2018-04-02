0d3641db9bb239dca7fc80334ddc652481bb05f9
Duc Do <dotrananhduc@gmail.com>
2018-04-02 16:50:30 +0700

    update with more data

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# with open('./ex1data2.txt') as f:
-#     data = f.read().splitlines()
-
-# data = [s.split(',') for s in data]
-
-# x_val = np.array([int(xv[1]) for xv in data])
-# y_val = np.array([int(yv[2]) for yv in data])
-
-
-
-def normalizeFeature(b0, b1, db0, db1):
-    learning_rate = 0.001
-    b_0 = b0 - learning_rate * db0
-    b_1 = b1 - learning_rate * db1
-    return (b_0, b_1)
-
 def computeCost(x_train, y_origin, b00, b11): # loss funtion
     sum = 0
     for i in range(len(x_train)):
@@ -38,21 +22,19 @@ def gradientDescent(x_train, y_origin, b0, b1, learning_rate):
     b_1 = b1 - (learning_rate * db1)
 
     return b_0, b_1
-    # return normalizeFeature(b0, b1, db0, db1)
+
 
 if __name__ == '__main__':
-    b0, b1 = 0, 0
+    b0, b1 = -34, 0.5999
     lr = 0.00001
 
-    x_val = np.array([147, 150, 153])
-    y_val = np.array([49, 50, 51])
-    print('Loss 1st: ' + str(computeCost(x_val, y_val, b0, b1)))
-    for it in range(10):
-        
-        # print('Loss: ' + str(computeCost(x_val, y_val, b0, b1)))
+    x_val = np.array([147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183])
+    y_val = np.array([49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68])
+    print('1st Loss: ' + str(computeCost(x_val, y_val, b0, b1)))
+    for it in range(100000):
         b0, b1 = gradientDescent(x_val, y_val, b0, b1, lr)
             
-    print('Loss last: ' + str(computeCost(x_val, y_val, b0, b1)))
+    print('Last Loss: ' + str(computeCost(x_val, y_val, b0, b1)))
     print('B0: ' + str(b0) + ' ' + 'B1: ' + str(b1))
 
     x0 = np.linspace(145, 185, 2)
